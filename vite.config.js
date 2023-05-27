db053d5b980e3c314c330c9f4deda83d30d3ef65
GuluGulu <2295980270@qq.com>
2023-05-27 22:36:41 +0800

    完成联想设置

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   resolve: {
+    //实质的@符转换
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
