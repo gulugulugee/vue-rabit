3e9d7c85d7a479b73fbe37a47a54a316af0d313e
GuluGulu <2295980270@qq.com>
2023-05-28 12:14:43 +0800

    axios技术封装

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,39 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+//elementPlus按需导入 通用ui
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    ////elementPlus按需导入
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [
+        //配置elementPlus采用sass样式配色系统
+        ElementPlusResolver({importStyle:"sass"}),
+      ],
+    }),
+  ],
   resolve: {
     //实质的@符转换
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
     }
   }
 })
