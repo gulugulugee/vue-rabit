344593ca6b04a5e0bf2db2f8ca51c6f758b022f8
GuluGulu <2295980270@qq.com>
2023-06-08 21:50:53 +0800

    token封装在拦截器中

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,6 +3,7 @@
 import axios from 'axios'
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
+import {useUserStore} from '@/stores/user'
 const httpInstance = axios.create({
   baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
   timeout: 5000
@@ -10,6 +11,14 @@ const httpInstance = axios.create({
 //拦截器
 // axios请求拦截器
 httpInstance.interceptors.request.use(config => {
+    // 拦截器携带token，目的是在接口中不用重复书写,请求需要携带token才能得到数据
+    // 1.从pinia中获取token数据
+    const useStore = useUserStore()
+    const token = useStore.userinfo.token
+    // 2.按后端要求拼接token数据
+    if(token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
     //发生请求前的处理
     return config
   }, e => Promise.reject(e))
