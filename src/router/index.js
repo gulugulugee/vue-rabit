81e8ec80755ff173fb5e8cddd8bc0fd2fb9a8a61
GuluGulu <2295980270@qq.com>
2023-06-10 22:30:06 +0800

    更好的router书写

--- src/router/index.js
+++ src/router/index.js
@@ -1,42 +1,51 @@
 import { createRouter, createWebHistory } from 'vue-router'
 //createRouter： 创建router实例对象
 //createWebHistory： 创建history模式的路由
-import layout from '@/views/layout/index.vue'
-import login from '@/views/login/index.vue'
-import home from '@/views/home/index.vue'
-import category from '@/views/category/index.vue'
-import subCategory from '@/views/subCategory/index.vue'
-import detail from '@/views/detail/index.vue'
+
+
+// import layout from '@/views/layout/index.vue'
+// import login from '@/views/login/index.vue'
+// import home from '@/views/home/index.vue'
+// import category from '@/views/category/index.vue'
+// import subCategory from '@/views/subCategory/index.vue'
+// import detail from '@/views/detail/index.vue'
+
+// maybe 更好的路由写法？
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path:'/',
-      component: layout,
+      component: () => import('@/views/layout/index.vue'),
       children:[
         {
           path:'',
-          component: home
+          // component: home
+          component: () => import('@/views/home/index.vue')
         },
         {
           //：id占位符
           path:'category/:id',
-          component: category
+          // component: category
+          component: () => import('@/views/category/index.vue')
         },
         {
           //：id占位符
           path:'category/sub/:id',
-          component: subCategory
+          // component: subCategory
+          component: () => import('@/views/subCategory/index.vue')
         },
         {
           path: 'detail/:id',
-          component: detail
+          // component: detail
+          component: () => import('@/views/detail/index.vue')
         }
       ]
     },
     {
       path:'/login',
-      component: login
+      // component: login
+      component: () => import('@/views/login/index.vue')
     }
   ],
 
